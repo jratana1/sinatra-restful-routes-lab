6dfe8572afc28a3097a0abf17b5bac102540881d
John Ratana <jratana1@gmail.com>
2020-11-25 18:44:51 -0500

    commit tree fe5fa4cee754812a78e3f3ea6405ea1451b777bb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,50 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+    get '/' do
+      erb :index
+    end
+
+    get '/recipes/new' do
+      erb :recipeform
+    end 
+
+    post '/recipes' do
+      @recipe = Recipe.create(params)
+      redirect "/recipes/#{@recipe.id}"
+    end
+
+    get '/recipes' do
+      @recipes = Recipe.all
+      erb :recipelist
+    end
+    
+    get '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      erb :recipeshow
+    end
+
+    get '/recipes/:id/edit' do
+      @recipe = Recipe.find(params[:id])
+      erb :edit
+    end
+
+    patch '/recipes/:id' do
+      recipe = Recipe.find(params[:id])
+      new_params ={}
+      new_params[:name] = params[:name]
+      new_params[:ingredients] = params[:ingredients]
+      new_params[:cook_time] = params[:cook_time]
+      recipe.update(new_params)
+  
+      redirect "/recipes/#{recipe.id}"
+    end
+
+    delete '/recipes/:id' do
+      recipe = Recipe.find(params[:id])
+      recipe.destroy
+      
+      redirect "/recipes"
+    end
 
 end
